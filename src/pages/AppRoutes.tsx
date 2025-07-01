a654129dd3e723ef0f4299fe7502a4e6cb4c0dce
Ivan <bloodshok96@gmail.com>
2025-07-01 16:00:58 +0300

    Update routing method to use createHashRouter and replace outdated script reference in index.html

--- src/pages/AppRoutes.tsx
+++ src/pages/AppRoutes.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import { LoginPage } from "./Login";
 import { DashboardPage } from "./Dashboard";
 import { Layout } from "./Layout";
 import { ForgotPasswordPage } from "./ForgotPassword";
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     element: <Layout />,
     children: [
